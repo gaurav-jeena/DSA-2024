9c9b60fcf72afac62f2d71058b0c48fcf953a7cd
GAURAV SINGH JEENA <gauravjeena2002@gmail.com>
2023-09-11 22:52:49 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,23 @@
-    int evenindex = binarysearch(even, 6, 12);
+#include <iostream>
+using namespace std;
 
-    cout << "The key is present at index " << evenindex << endl;
+int pivot(int arr[], int n) {
+    int s = 0;
+    int e = n - 1;
+    while (s < e) {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] >= arr[s]) {
+            s = mid + 1;
+        } else {
+            e = mid;
+        }
+    }
+    return s;
+}
+
+int main() {
+    int arr[5] = {7, 8, 5,1, 3};
+    int pivotIndex = pivot(arr, 5);
+    cout << "The pivot element is at index " << pivotIndex << endl;
+    return 0;
+}
